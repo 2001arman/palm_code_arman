a904f9784dbf43f061d8b242bc8ba8edf8f29e4d
arman <2001armanmaulana@gmail.com>
2025-06-30 00:02:10 +0700

    UI: add safearea to detail screen

--- lib/presentation/detail/detail_screen.dart
+++ lib/presentation/detail/detail_screen.dart
@@ -34,36 +34,38 @@ class DetailScreen extends StatelessWidget {
         ],
       ),
 
-      body: ListView(
-        padding: EdgeInsets.all(20),
-        children: [
-          CachedNetworkImage(
-            imageUrl: logic.book.formats.imageJpeg,
-            height: 300,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            logic.book.title,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: logic.book.subjects
-                    .map((data) => SubjectItem(subject: data))
-                    .toList(),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            CachedNetworkImage(
+              imageUrl: logic.book.formats.imageJpeg,
+              height: 300,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              logic.book.title,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: logic.book.subjects
+                      .map((data) => SubjectItem(subject: data))
+                      .toList(),
+                ),
               ),
             ),
-          ),
-          for (int i = 0; i < logic.book.summaries.length; i++)
-            Text(
-              logic.book.summaries[i],
-              style: TextStyle(color: Colors.black, fontSize: 14),
-              textAlign: TextAlign.justify,
-            ),
-        ],
+            for (int i = 0; i < logic.book.summaries.length; i++)
+              Text(
+                logic.book.summaries[i],
+                style: TextStyle(color: Colors.black, fontSize: 14),
+                textAlign: TextAlign.justify,
+              ),
+          ],
+        ),
       ),
     );
   }
